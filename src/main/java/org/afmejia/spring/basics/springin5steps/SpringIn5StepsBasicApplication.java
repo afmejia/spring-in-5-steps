6f24e27f83eef3f7e47ab78c235a3b142d894110
Felipe <afmejia23@utp.edu.co>
2019-07-04 05:09:12 -0500

    Removing Spring Framework

--- src/main/java/org/afmejia/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/org/afmejia/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,24 +1,25 @@
 package org.afmejia.spring.basics.springin5steps;
 
 import org.afmejia.spring.basics.springin5steps.basic.BinarySearchImpl;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
-		/*PersonDAO bynarySearch = new PersonDAO(new QuickSortAlgorithm());
-		int result = bynarySearch.binarySearch(new int[] {124, 6}, 3);
-		System.out.println(result);*/
-		// SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
-		
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		
-		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-		System.out.println(result);
+		try {
+			AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);
+			
+			
+			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+			
+			int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+			System.out.println(result);
+		} finally {}
 	}
 
 }
